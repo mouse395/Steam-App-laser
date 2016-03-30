c9554374e52425750fbf57032c7e8624a9a8e74a
rat395 <julio_parrales@hotmail.com>
2016-03-30 12:28:58 -0700

    added laser module

--- laserTripwire/laserTripwire.ino
+++ laserTripwire/laserTripwire.ino
@@ -2,7 +2,7 @@ int ldr = A4;
 int buzzer = 11;
 //led is also a digitalWrite so we use pins from 0 to 12
 int led = 4;
-
+int laser = 1;
 //no need for longs
 int ldrV1, ldrV2;
 
@@ -11,10 +11,13 @@ void setup() {
   pinMode (buzzer, OUTPUT);
   pinMode (led, OUTPUT);
   pinMode (ldr, INPUT);
+  pinMode (laser, OUTPUT);
  
 }
 
 void loop() {
+
+    digitalWrite(laser, HIGH);
 
     ldrV1 = analogRead(ldr);
     delay(10);
